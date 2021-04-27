e7d50a83fc3b5c5e2c28b3a71670440d7cfe109b
Egor Ponomaryov <egorponomarev93@gmail.com>
2021-04-27 14:01:03 +0300

    Fix.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,10 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
     id("com.android.application")
     kotlin("android")
-    // kotlin("kapt")
-    // id("kotlin-android")
     id("androidx.navigation.safeargs")
     id("kotlin-parcelize")
-    id("kotlin-android")
     kotlin("kapt")
 }
-
-//apply(plugin = "kotlin-kapt")
 
 android {
     compileSdkVersion(30)
@@ -49,17 +42,17 @@ android {
 
 dependencies {
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.31")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     // ViewMode and LiveData
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.preference:preference-ktx:1.1.1")
@@ -71,15 +64,15 @@ dependencies {
     implementation("com.healbe:healbesdk:1.0.7") { isTransitive = true }
 
     // Kotlin Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.4.2")
 
     // LiveDataReactiveStreams
-    implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:2.3.1")
 
     // Ktor
-    implementation("io.ktor:ktor-client-core:1.5.2")
-    implementation("io.ktor:ktor-client-cio:1.5.2")
+    implementation("io.ktor:ktor-client-core:1.5.3")
+    implementation("io.ktor:ktor-client-cio:1.5.3")
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
@@ -91,11 +84,11 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     // Room Database
-    implementation("androidx.room:room-runtime:2.2.6")
-    kapt("androidx.room:room-compiler:2.2.6")
+    implementation("androidx.room:room-runtime:2.3.0")
+    kapt("androidx.room:room-compiler:2.3.0")
 
     // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.2.6")
+    implementation("androidx.room:room-ktx:2.3.0")
 
     // Timber log
     implementation("com.jakewharton.timber:timber:4.7.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ buildscript {
     extra["kotlin_version"] = "1.4.31"
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("com.android.tools.build:gradle:4.2.0-rc01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlin_version"]}")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -17,7 +17,7 @@ buildscript {
 allprojects {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
         maven("https://dl.bintray.com/healbe/healbesdk")
         maven("https://dl.bintray.com/skeptick/maven")
         maven("https://jitpack.io")
